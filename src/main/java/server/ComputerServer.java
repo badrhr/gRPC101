80a62de515d0f5adf2b8cc52836161bcf3c8d4db
xproce <badr.hirchoua@gmail.com>
2024-11-25 15:53:08 +0100

    commit

--- src/main/java/server/ComputerServer.java
+++ src/main/java/server/ComputerServer.java
@@ -1,2 +1,18 @@
-package server;public class ComputerServer {
+package server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import service.ComputerService;
+
+import java.io.IOException;
+
+public class ComputerServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(9090)
+                .addService(new ComputerService())
+                .build();
+
+        server.start();
+        server.awaitTermination();
+    }
 }
--- src/main/java/service/ComputerService.java
+++ src/main/java/service/ComputerService.java
@@ -1,2 +1,31 @@
-package service;public class ComputerService {
+package service;
+
+import io.grpc.stub.StreamObserver;
+import org.xproce.labs.Computer;
+import org.xproce.labs.ComputerServiceGrpc;
+
+public class ComputerService extends ComputerServiceGrpc.ComputerServiceImplBase {
+
+    @Override
+    public void addComputer(Computer.ComputerRequest request,
+                            StreamObserver<Computer.ComputerResponse> responseObserver) {
+        String name = request.getName();
+        String caracteriques = request.getCaracterique();
+        double price = request.getPrice();
+
+        //ComputerRepository -> save() ->   id
+
+        int id = 12345;
+
+
+        Computer.ComputerResponse response = Computer.ComputerResponse.newBuilder()
+                .setId(id)
+                .setName(name)
+                .setCaracterique(caracteriques)
+                .setPrice(price)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
